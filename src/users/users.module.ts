6f96c48c7f3bfe2fc0d776d2fa8a13846b0dff4f
BroGames <hunempire11@gmail.com>
2025-03-10 10:12:43 +0100

    updated jwt secret names

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -13,8 +13,8 @@ import { JwtModule } from '@nestjs/jwt';
         inject: [ConfigService],
         useFactory: (configService: ConfigService) => ({
           global: true,
-          secret: configService.get<string>('JWT_SECRET'),
-          signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN')},
+          secret: configService.get<string>('JWT_ACCESS_SECRET'),
+          signOptions: { expiresIn: configService.get<string>('JWT_ACCESS_EXPIRES_IN')},
         })
       })
     ],
