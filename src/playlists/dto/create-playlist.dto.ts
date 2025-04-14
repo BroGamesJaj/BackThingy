649148337a7bcd90ddbc933abe5a5e37a610819f
Mészáros Botond <m.botond07@gmail.com>
2025-04-14 16:07:23 +0200

    playlist doc

--- src/playlists/dto/create-playlist.dto.ts
+++ src/playlists/dto/create-playlist.dto.ts
@@ -1,11 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreatePlaylistDto {
+
+    @ApiProperty({
+        description: "The name of the playlist",
+        example: "ExamplePlaylist"
+    })
     @IsNotEmpty()
     @IsString()
     PlaylistName: string;
     
     Owner: null;
+
+    @ApiProperty({
+        description: "The description of the playlist",
+        example: "Example description",
+        required: false
+    })
     Description: string | null;
+
+    @ApiProperty({
+        description: "The property that defines the playlists visibility",
+        example: "true"
+    })
     Private: string;
 }
--- src/playlists/entities/playlist.entity.ts
+++ src/playlists/entities/playlist.entity.ts
@@ -1,10 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class Playlist {
+    @ApiProperty({
+        example: 2
+    })
     PlaylistID: number;
+
+    @ApiProperty({
+        example: 13
+    })
     OwnerID: number;
+
+    @ApiProperty({
+        example: "ExamplePlaylist"
+    })
     PlaylistName: string;
+
+    @ApiProperty({
+        example: "Example description",
+        required: false
+    })
     Description: string | null;
+
+    @ApiProperty({
+        description: 'Image file data encoded as a Base64 string',
+        example: { 'image/png': { schema: { type: 'string', format: 'binary' } } },
+        required: false
+    })
     PlaylistCover: Buffer | null;
+
+    @ApiProperty({
+        example: true,
+    })
     Private: boolean;
+
+    @ApiProperty({
+        example: [],
+    })
     Tracks: {
         PlaylistID: number;
         TrackID: number;
