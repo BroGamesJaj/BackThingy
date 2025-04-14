3d535ece5d0f2841da4c83cd055f1baf05f6b4e8
Mészáros Botond <m.botond07@gmail.com>
2025-04-14 16:06:57 +0200

    auth doc

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class signInDto{
+    @ApiProperty({
+        description: "The email of the user",
+        example: "example@email.com"
+    })
     Email: string;
+
+    @ApiProperty({
+        description: "The password of the user",
+        example: "examplePassword123"
+    })
     Password: string;
 }
