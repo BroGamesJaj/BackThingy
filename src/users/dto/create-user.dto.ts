bf39cd2b90db2e1a7e910d7a726110974f0616a2
Mészáros Botond <m.botond07@gmail.com>
2025-04-14 16:07:38 +0200

    user doc

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({
+        description: "The users email address",
+        example: "example@email.com"
+    })
     @IsNotEmpty()
     @IsEmail()
     Email: string;
 
+    @ApiProperty({
+        description: "The users user name",
+        example: "Example User"
+    })
     @IsNotEmpty()
     @IsString()
     Username: string;
 
+    @ApiProperty({
+        description: "The users password",
+        example: "examplePassword123"
+    })
     @IsNotEmpty()
     @IsString()
     Password: string;
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class User {
+    @ApiProperty({
+        example: 1
+    })
+    UserID: number;
+
+    @ApiProperty({
+        example: "Example User"
+    })
+    Username: string;
+
+    @ApiProperty({
+        example: "example@email.com"
+    })
+    Email: string;
+
+    @ApiProperty({
+        description: 'Image file data encoded as a Base64 string',
+        example: { 'image/png': { schema: { type: 'string', format: 'binary' } } },
+    })
+    Pfp: Buffer;
+
+    @ApiProperty({
+        example: "example description"
+    })
+    Description: string;
+}
