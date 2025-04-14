5ee188c70d53b8f073e3a97cf720a8c58a3c9a5b
Mészáros Botond <m.botond07@gmail.com>
2025-04-14 16:07:08 +0200

    follow doc

--- src/followed/dto/create-followed.dto.ts
+++ src/followed/dto/create-followed.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateFollowedDto {
+    @ApiProperty({
+        description: "The id of the followed item",
+        example: 3
+    })
     FollowedID: number;
+
+    @ApiProperty({
+        description: "The type of the followed item",
+        example: "Playlist"
+    })
     Type: "Playlist" | "Album" | "Artist";
 }
--- src/followed/entities/followed.entity.ts
+++ src/followed/entities/followed.entity.ts
@@ -1,1 +1,30 @@
-export class Followed {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class Followed {
+    @ApiProperty({
+        example: 32
+    })
+    FollowedID: number;
+
+    @ApiProperty({
+        example: 512
+    })
+    UserID: number;
+
+    @ApiProperty({
+        example: 5423,
+        required: false
+    })
+    PlaylistID?: number;
+
+    @ApiProperty({
+        example: 12324,
+        required: false
+    })
+    TypeID?: number;
+
+    @ApiProperty({
+        example: "Album",
+    })
+    Type: string;
+}
